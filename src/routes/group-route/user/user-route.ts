d6898fa50ef1062d8d61c4bedc9bc99fdd831720
igorfals <igorfeals@gmail.com>
2023-02-05 21:34:43 -0300

    feat: Autorizando rotas de usuario

--- src/routes/group-route/user/user-route.ts
+++ src/routes/group-route/user/user-route.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import { UserMiddleWare } from '../../../middlewares/user'
+import { AuthMiddleWare } from '../../../middlewares/auth'
 
 const router = Router()
 const userMiddleWere = new UserMiddleWare()
+const authMiddleWare = new AuthMiddleWare()
 
-router.post('/add', userMiddleWere.setUser)
-router.get('/', userMiddleWere.getUser)
-router.put('/update', userMiddleWere.updateUser)
-router.delete('/delete/:id', userMiddleWere.deleteUser)
+router.post('/add', authMiddleWare.authorize, userMiddleWere.setUser)
+router.get('/', authMiddleWare.authorize, userMiddleWere.getUser)
+router.put('/update', authMiddleWare.authorize, userMiddleWere.updateUser)
+router.delete('/delete/:id', authMiddleWare.authorize, userMiddleWere.deleteUser)
 
 export default router
