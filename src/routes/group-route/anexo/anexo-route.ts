c90fcf42b413bb7c99d294e36df9b3b731f5813d
igorfals <igorfeals@gmail.com>
2023-02-08 18:10:51 -0300

    feat: Anexo: Criando autorização

--- src/routes/group-route/anexo/anexo-route.ts
+++ src/routes/group-route/anexo/anexo-route.ts
@@ -1,14 +1,16 @@
 import { Router } from 'express'
 import { AnexoMiddleWare } from '../../../middlewares/anexo'
 import { alternativeUpload } from '../../../helpers/upload'
+import { AuthMiddleWare } from '../../../middlewares/auth'
 
 const router = Router()
 const upload = alternativeUpload()
 const anexoMiddleWare = new AnexoMiddleWare()
+const authMiddleWare = new AuthMiddleWare()
 
-router.post('/anexo-upload', upload.single('anexo'), anexoMiddleWare.setAnexo)
-router.get('/add', anexoMiddleWare.getAnexo)
-router.put('/update', upload.single('anexo'), anexoMiddleWare.updateAnexo)
-router.delete('/delete/:id', anexoMiddleWare.deleteAnexo)
+router.post('/anexo-upload', authMiddleWare.authorize, upload.single('anexo'), anexoMiddleWare.setAnexo)
+router.get('/add', authMiddleWare.authorize, anexoMiddleWare.getAnexo)
+router.put('/update', authMiddleWare.authorize, upload.single('anexo'), anexoMiddleWare.updateAnexo)
+router.delete('/delete/:id', authMiddleWare.authorize, anexoMiddleWare.deleteAnexo)
 
 export default router
