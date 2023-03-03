120dde79a2078e06dc8bc39eab2b409225c19ee1
igorfals <igorfeals@gmail.com>
2023-03-03 15:17:41 -0300

    feat: documentando rota de usuario

---
+++ src/routes/group-route/categoria/doc-categoria.ts
@@ -0,0 +1,21 @@
+/**
+ * @swagger
+ *  components:
+ *   schemas:
+ *     UsersAdd:
+ *       type: object
+ *       required:
+ *         - nome
+ *         - email
+ *         - useraname
+ *         - password 
+ *       properties:
+ *         nome:
+ *           type: string
+ *         email:
+ *           type: string
+ *         username:
+ *           type: string
+ *         password:
+ *           type: string
+ */
