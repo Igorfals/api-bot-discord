ecd5c3e9adea61b691d671246e8ebac8183e7405
igorfals <igorfeals@gmail.com>
2023-03-06 14:09:51 -0300

    feat: doc user

--- src/routes/group-route/user/doc-user.ts
+++ src/routes/group-route/user/doc-user.ts
@@ -98,6 +98,25 @@
  *    description: "Rota responsável por listar os usuarios."
  *    tags:
  *      - "users"
+ *    parameters:
+ *      - name: pesquisa
+ *        in: query
+ *        description: "string para pesquisar musica"
+ *        required: false
+ *        schema:
+ *          type: string
+ *      - name: limit
+ *        in: query
+ *        description: "Limite de itens para paginação"
+ *        required: false
+ *        schema:
+ *          type: number
+ *      - name: offset
+ *        in: query
+ *        description: "Quantidade de itens para ignorar"
+ *        required: false
+ *        schema:
+ *          type: number
  *    security:
  *      - ApiKeyAuth: []
  *    responses:
